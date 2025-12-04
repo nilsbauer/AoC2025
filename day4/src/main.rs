3c38eb7a3dfe0bc1598f38b986386ae6be2726dc
nba <nils.bauer@proetcon.de>
2025-12-04 09:14:55 +0100

    day 4 star 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,16 +3,26 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input1.txt").unwrap();
-    let input : Vec<Vec<char>> = input.lines().map(|s| s.chars().collect()).collect();
+    let mut input : Vec<Vec<char>> = input.lines().map(|s| s.chars().collect()).collect();
     let mut res = 0;
 
-    for (row_idx, row) in input.iter().enumerate() {
-        for (col_idx, cell) in row.iter().enumerate() {
-            if *cell == '@' && count_adjacent(&input, row_idx, col_idx) < 4 {
-                res += 1;
+    loop {
+        let mut next_input = input.clone();
+        let mut removed_now = 0;
+        for (row_idx, row) in input.iter().enumerate() {
+            for (col_idx, cell) in row.iter().enumerate() {
+                if *cell == '@' && count_adjacent(&input, row_idx, col_idx) < 4 {
+                    removed_now += 1;
+                    next_input[row_idx][col_idx] = 'x';
+                }
             }
         }
+        if removed_now == 0 { break; }
+        println!("removed {removed_now} rolls");
+        res += removed_now;
+        input = next_input;
     }
+    println!("----------------");
     println!("{res}");
 }
 
