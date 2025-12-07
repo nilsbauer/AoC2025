45aaa8da63acba4d052f60300622a2f2548a855e
nilsb <nilsbauer@outlook.de>
2025-12-07 16:59:39 +0100

    day 6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -3,28 +3,46 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input1.txt").unwrap();
-    let mut nums : Vec<Vec<u128>> = Vec::new();
+    let mut nums : Vec<u128> = Vec::new();
     let mut res = 0;
 
     for line in input.lines() {
-        for (idx, col) in line.split_ascii_whitespace().enumerate() {
-            if col.trim() == "+" {
-                res += nums[idx].iter().fold(0, |acc, x| acc + x);
-                continue;
-            }
-            if col.trim() == "*" {
-                res += nums[idx].iter().fold(1, |acc, x| acc * x);
-                continue;
-            }
-            let col : u128 = col.parse().unwrap();
-            if let Some(col_list) = nums.get_mut(idx) {
-                col_list.push(col);
-            } else {
-                let col_list = vec![col];
-                nums.push(col_list);
+        if nums.len() < line.len() {
+            nums.resize(line.len(), 0);
+        }
+        for (idx, col) in line.chars().enumerate() {
+            if let Some(digit) = col.to_digit(10) {
+                if let Some(col_val) = nums.get_mut(idx) {
+                    *col_val = *col_val * 10 + u128::from(digit);
+                }
+            } else if col == '+' {
+                let mut i = idx;
+                let mut term = 0;
+                loop {
+                    if i >= nums.len() || nums[i] == 0 {
+                        break;
+                    } else {
+                        term += nums[i];
+                    }
+                    i += 1;
+                }
+                res += term;
+            } else if col == '*' {
+                let mut i = idx;
+                let mut term = 1;
+                loop {
+                    if i >= nums.len() || nums[i] == 0 {
+                        break;
+                    } else {
+                        term *= nums[i];
+                    }
+                    i += 1;
+                }
+                res += term;
             }
         }
     }
+
 
     println!("{res}");
 }
