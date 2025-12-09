729840494d798cfaaf7e94cba978878d057e5021
nba <nils.bauer@proetcon.de>
2025-12-09 15:27:25 +0100

    day 9 star 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -16,9 +16,7 @@ fn main() {
     for (idx1, (x1, y1)) in polygon.points.iter().enumerate() {
         for (x2, y2) in polygon.points.iter().skip(idx1+1) {
             let (x1, x2, y1, y2) = (*x1, *x2, *y1, *y2);
-            let (x1, x2) = if x1 < x2 { (x1, x2) } else { (x2, x1) };
-            let (y1, y2) = if y1 < y2 { (y1, y2) } else { (y2, y1) };
-            if !polygon.contains_square(x1, x2, y1, y2) {
+            if !polygon.contains_rectangle(x1, x2, y1, y2) {
                 continue;
             }
             let area = (x1.abs_diff(x2) + 1) * (y1.abs_diff(y2) + 1);
@@ -33,66 +31,99 @@ fn main() {
 
 struct Polygon {
     points: Vec<(u64, u64)>,
+    sides: Vec<LineSegment>
 }
 
 impl Polygon {
     fn from_point_vec(points: Vec<(u64, u64)>) -> Polygon {
-        Polygon { points }
+        let mut sides = Vec::new();
+        let mut last_point = None;
+        for (x, y) in &points {
+            if let Some((last_x, last_y)) = last_point {
+                sides.push(LineSegment::new(*x, last_x, *y, last_y));
+            }
+            last_point = Some((*x, *y));
+        }
+        let (x1, y1) = points[0];
+        let (x2, y2) = points[points.len()-1];
+        sides.push(LineSegment::new(x1, x2, y1, y2));
+
+        Polygon { points, sides }
     }
 
-    fn contains_square(&self, x1: u64, x2: u64, y1: u64, y2: u64) -> bool {
-        if x1 > x2 || y1 > y2 {
-            panic!("square is not ordered!");
-        }
+    fn contains_rectangle(&self, x1: u64, x2: u64, y1: u64, y2: u64) -> bool {
+        self.contains_point(x1, y2) &&
+        self.contains_point(x2, y1) &&
+        !self.sides.iter().any(|s| s.intersects_rectangle(x1, x2, y1, y2))
+    }
 
-        for (idx, (x, y)) in self.points.iter().enumerate() {
-            let (x, y) = (*x, *y);
-            if x1 < x && x < x2 && y1 < y && y < y2 {
-                return false;
-            }
-            if y1 < y && y < y2 {
-                if x == x1 {
-                    let idx_before = if idx > 0 { idx-1 } else { self.points.len() - 1 };
-                    let idx_after = if idx < self.points.len() - 1 { idx+1 } else { 0 };
-                    let (x_before, _) = self.points[idx_before];
-                    let (x_after, _) = self.points[idx_after];
-                    if x_before > x || x_after > x {
-                        return false;
+    fn contains_point(&self, x: u64, y: u64) -> bool {
+        let mut intersections = 0;
+        for side in &self.sides {
+            if side.direction == Direction::Horizontal {
+                if side.y1 < y {
+                    if side.x1 <= x && side.x2 > x {
+                        intersections += 1;
                     }
-                }
-                if x == x2 {
-                    let idx_before = if idx > 0 { idx-1 } else { self.points.len() - 1 };
-                    let idx_after = if idx < self.points.len() - 1 { idx+1 } else { 0 };
-                    let (x_before, _) = self.points[idx_before];
-                    let (x_after, _) = self.points[idx_after];
-                    if x_before < x || x_after < x {
-                        return false;
-                    }
-                }
-            }
-            if x1 < x && x < x2 {
-                if y == y1 {
-                    let idx_before = if idx > 0 { idx-1 } else { self.points.len() - 1 };
-                    let idx_after = if idx < self.points.len() - 1 { idx+1 } else { 0 };
-                    let (_, y_before) = self.points[idx_before];
-                    let (_, y_after) = self.points[idx_after];
-                    if y_before > y || y_after > y {
-                        return false;
-                    }
-                }
-                if y == y2 {
-                    let idx_before = if idx > 0 { idx-1 } else { self.points.len() - 1 };
-                    let idx_after = if idx < self.points.len() - 1 { idx+1 } else { 0 };
-                    let (_, y_before) = self.points[idx_before];
-                    let (_, y_after) = self.points[idx_after];
-                    if y_before < y || y_after < y {
-                        return false;
-                    }
+                } else if side.y1 == y && side.x1 <= x && side.x2 >= x {
+                    // point is on the vertex
+                    return true;
                 }
             }
         }
-        true
+        intersections % 2 == 1
     }
+}
+
+#[derive(Debug)]
+struct LineSegment {
+    x1: u64,
+    x2: u64,
+    y1: u64,
+    y2: u64,
+    direction: Direction,
+}
+
+impl LineSegment {
+    fn new(x1: u64, x2: u64, y1: u64, y2: u64) -> Self {
+        let (x1, x2) = if x1 < x2 { (x1, x2) } else { (x2, x1) };
+        let (y1, y2) = if y1 < y2 { (y1, y2) } else { (y2, y1) };
+        let direction = if x1 == x2 {
+            Direction::Vertical
+        } else {
+            Direction::Horizontal
+        };
+
+        LineSegment { x1, x2, y1, y2, direction }
+    }
+
+    fn intersects_rectangle(&self, x1: u64, x2: u64, y1: u64, y2: u64) -> bool {
+        let (x1, x2) = if x1 < x2 { (x1, x2) } else { (x2, x1) };
+        let (y1, y2) = if y1 < y2 { (y1, y2) } else { (y2, y1) };
+
+        match self.direction {
+            Direction::Vertical => {
+                if self.x1 <= x1 || self.x1 >= x2 {
+                    false
+                } else {
+                    !(self.y1 <= y1 && self.y2 <= y1 || self.y1 >= y2 && self.y2 >= y2)
+                }
+            },
+            Direction::Horizontal => {
+                if self.y1 <= y1 || self.y1 >= y2 {
+                    false
+                } else {
+                    !(self.x1 <= x1 && self.x2 <= x1 || self.x1 >= x2 && self.x2 >= x2)
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug,PartialEq,Eq)]
+enum Direction {
+    Horizontal,
+    Vertical,
 }
 
 #[cfg(test)]
@@ -102,6 +133,12 @@ mod tests {
     #[test]
     fn test_contains_square() {
         let polygon = Polygon::from_point_vec(vec![(7, 1), (11, 1), (11, 7), (9, 7), (9, 5), (2, 5), (2, 3), (7, 3)]);
-        assert!(polygon.contains_square(2, 9, 3, 5));
+        assert!(polygon.contains_rectangle(2, 9, 3, 5));
+    }
+
+    #[test]
+    fn test_intersection() {
+        let line = LineSegment::new(1, 3, 2, 2);
+        assert!(line.intersects_rectangle(1, 3, 1, 3));
     }
 }
