58e69afc71eba12cbcee0b3a16885c6350a044d1
nilsb <nilsbauer@outlook.de>
2025-12-10 23:37:59 +0100

    wip

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -10,8 +10,9 @@ fn main() {
     let mut res = 0;
 
     for line in input.lines() {
-        let state : ButtonState = line.parse().unwrap();
+        let mut state : ButtonState = line.parse().unwrap();
         if let Some(num_presses) = state.solve() {
+            println!("solved in {num_presses} presses");
             res += num_presses;
         } else {
             panic!("couldn't solve");
@@ -26,54 +27,47 @@ struct ButtonState {
     presses: Vec<u32>,
     joltage_targets: Vec<u32>,
     current_joltage: Vec<u32>,
-    possible_button_presses: Vec<u32>,
 }
 
 impl ButtonState {
     fn determine_next_button(&self) -> Option<usize> {
-        let mut biggest_button_size = self.buttons[0].len();
         for button_size in (0 ..= self.buttons[0].len()).rev() {
-
-            if let Some(ret, _, _) =
+            if let Some((ret, _, _)) =
                 self.buttons.iter()
-                    .zip(self.possible_button_presses)
+                    .zip(self.calc_possible_button_presses())
                     .enumerate()
                     .map(|(idx, (button, poss_presses))| (idx, button, poss_presses))
                     .filter(|(_, b, _)| b.len() == button_size)
-                    .max_by_key(|(_, _, p)| p)
+                    .max_by_key(|(_, _, p)| *p)
             {
-                return Some(ret);
+                if ret > 0 {
+                    return Some(ret);
+                }
             }
         }
         None
     }
 
-    fn solve(&self) -> Option<u32> {
-        for num_presses in 0..13 {
-            println!("trying to solve with {num_presses} presses");
-            if self.solve_with_n_presses(num_presses) {
-                println!("found a solution with {num_presses} presses");
-                return Some(num_presses.try_into().unwrap());
-            }
+    fn solve(&mut self) -> Option<u32> {
+        let mut ret = 0;
+        while let Some(next_button) = self.determine_next_button() {
+            println!("trying to press button {next_button}");
+            self.press_button(next_button);
+            println!("joltages: {:?}", self.current_joltage);
+            ret += 1;
         }
-        None
+        if self.cmp_joltage() {
+            Some(ret)
+        } else {
+            None
+        }
     }
 
-    fn solve_with_n_presses(&self, remaining_presses: usize) -> bool {
-        if remaining_presses == 0 {
-            return self.cmp_joltage()
+    fn press_button(&mut self, idx: usize) {
+        self.presses[idx] += 1;
+        for joltage in &self.buttons[idx] {
+            self.current_joltage[*joltage] += 1;
         }
-        if self.exceeds_joltage() {
-            return false;
-        }
-        for next_press in 0..self.buttons.len() {
-            let mut next_state = self.clone();
-            next_state.presses[next_press] += 1;
-            if next_state.solve_with_n_presses(remaining_presses-1) {
-                return true;
-            }
-        }
-        false
     }
 
     fn cmp_joltage(&self) -> bool {
@@ -86,14 +80,25 @@ impl ButtonState {
         joltages.iter().zip(self.joltage_targets.iter()).all(|(joltage, target)| *joltage == *target)
     }
 
-    fn exceeds_joltage(&self) -> bool {
-        let mut joltages = vec![0; self.joltage_targets.len()];
-        for (button_joltages, presses) in self.buttons.iter().zip(self.presses.iter()) {
-            for button_joltage in button_joltages {
-                joltages[*button_joltage] += presses;
-            }
-        }
-        joltages.iter().zip(self.joltage_targets.iter()).any(|(joltage, target)| *joltage > *target)
+    //fn exceeds_joltage(&self) -> bool {
+    //    let mut joltages = vec![0; self.joltage_targets.len()];
+    //    for (button_joltages, presses) in self.buttons.iter().zip(self.presses.iter()) {
+    //        for button_joltage in button_joltages {
+    //            joltages[*button_joltage] += presses;
+    //        }
+    //    }
+    //    joltages.iter().zip(self.joltage_targets.iter()).any(|(joltage, target)| *joltage > *target)
+    //}
+
+    fn calc_possible_button_presses(&self) -> Vec<u32> {
+        let ret = self.buttons.iter()
+            .map(|b| b.iter()
+                .map(|j| self.joltage_targets[*j] - self.current_joltage[*j])
+                .min()
+                .unwrap())
+            .collect();
+        println!("possible: {ret:?}");
+        ret
     }
 }
 
@@ -122,19 +127,34 @@ impl FromStr for ButtonState {
             })
         .collect();
         buttons.sort_by_key(|b| b.len());
+        buttons.reverse();
         let presses = vec![0; buttons.len()];
 
-        let joltage_targets = joltage.split(',').map(|j| j.parse().unwrap()).collect();
+        let joltage_targets : Vec<_> = joltage.split(',').map(|j| j.parse().unwrap()).collect();
         let current_joltage = vec![0; joltage_targets.len()];
-        let possible_button_presses = calc_possible_button_presses(buttons, joltage_targets);
-        Ok(ButtonState { buttons, presses, joltage_targets, current_joltage, possible_button_presses  })
+        println!("buttons: {buttons:?}");
+        Ok(ButtonState { buttons, presses, joltage_targets, current_joltage })
     }
-}
-
-fn calc_possible_button_presses(buttons: &Vec<Vec<usize>>, joltage_targets: &Vec<u32>) -> Vec<u32> {
-    buttons.map(|b| b.map(|j| joltage_target[j]).min().unwrap()).collect()
 }
 
 #[derive(Debug, PartialEq, Eq)]
 enum ButtonStateParseError {
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tr() {
+        let buttons = vec![[3], [2], [1, 3], [2, 3], [0, 2], [0, 1]];
+        let possible = vec![7, 4, 5, 4, 3, 3];
+        let button_size = 1;
+        self.buttons.iter()
+            .zip(self.calc_possible_button_presses())
+            .enumerate()
+            .map(|(idx, (button, poss_presses))| (idx, button, poss_presses))
+            .filter(|(_, b, _)| b.len() == button_size)
+            .max_by_key(|(_, _, p)| *p)
+    }
 }
