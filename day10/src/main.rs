154c33572d1ebacf37b0d3c7a8f85674f5e96823
nba <nils.bauer@proetcon.de>
2025-12-11 17:02:23 +0100

    wip

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -145,16 +145,57 @@ enum ButtonStateParseError {
 mod tests {
     use super::*;
 
+    //#[test]
+    //fn tr() {
+    //    let buttons = vec![[3], [2], [1, 3], [2, 3], [0, 2], [0, 1]];
+    //    let possible = vec![7, 4, 5, 4, 3, 3];
+    //    let button_size = 1;
+    //    self.buttons.iter()
+    //        .zip(self.calc_possible_button_presses())
+    //        .enumerate()
+    //        .map(|(idx, (button, poss_presses))| (idx, button, poss_presses))
+    //        .filter(|(_, b, _)| b.len() == button_size)
+    //        .max_by_key(|(_, _, p)| *p)
+    //}
+
     #[test]
-    fn tr() {
-        let buttons = vec![[3], [2], [1, 3], [2, 3], [0, 2], [0, 1]];
-        let possible = vec![7, 4, 5, 4, 3, 3];
-        let button_size = 1;
-        self.buttons.iter()
-            .zip(self.calc_possible_button_presses())
-            .enumerate()
-            .map(|(idx, (button, poss_presses))| (idx, button, poss_presses))
-            .filter(|(_, b, _)| b.len() == button_size)
-            .max_by_key(|(_, _, p)| *p)
+    fn tr2() {
+        let data = vec![vec![0,2,3,4],vec![2,3],vec![0,4],vec![0,1,2],vec![1,2,3,4]];
+        get_next_idx(&data, Vec::new());
+    }
+
+    fn get_next_idx(data: &Vec<Vec<usize>>, factors: Vec<i32>) {
+        if factors.len() < 5 {
+            for factor in -1..=2 {
+                let mut factors = factors.clone();
+                factors.push(factor);
+                get_next_idx(data, factors);
+            }
+        } else {
+            for (idx, factor) in factors.iter().enumerate() {
+                let term = match factor {
+                    -1 => format!("- x{idx} "),
+                    0  => format!("+0x{idx} "),
+                    1  => format!("+ x{idx} "),
+                    f  => format!("+ {f}x{idx} "),
+                };
+                print!("{term}");
+            }
+            print!(" = ");
+            let mut result = vec![0; 5];
+            for (factor, data) in factors.iter().zip(data.iter()) {
+                for d in data {
+                    result[*d] += factor;
+                }
+            }
+            for res in &result {
+                print!("{res:>2} ");
+            }
+            let num0 = result.iter().fold(0, |acc, x| if *x == 0 { acc + 1} else { acc });
+            if num0 == 4 {
+                print!("HIERHIERHIER");
+            }
+            println!("   | {}", num0);
+        }
     }
 }
--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,26 +1,60 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fs;
 
 
 fn main() {
-    let input = fs::read_to_string("input.txt").unwrap();
+    let input = fs::read_to_string("input1.txt").unwrap();
     let mut nodes = HashMap::new();
     for line in input.lines() {
         let (node, connections) = line.split_once(':').unwrap();
         let connections: Vec<_> = connections.split_ascii_whitespace().map(|s| s.to_string()).collect();
-        nodes.insert(node.to_string(), connections);
+        nodes.insert(node.to_string(), Node::new(connections));
     }
-    let ret = find_out(&nodes, "you");
+    let ret = find_out(&mut nodes, "svr", false, false);
     println!("{ret}");
 }
 
-fn find_out(nodes: &HashMap<String, Vec<String>>, current_node: &str) -> u32 {
+fn find_out(nodes: &HashMap<String, Node>, current_node: &str, visited_dac: bool, visited_fft: bool) -> u32 {
+    let mut visited_dac = visited_dac;
+    let mut visited_fft = visited_fft;
+
     if current_node == "out" {
-        return 1;
+        if visited_dac && visited_fft {
+            return 1;
+            println!("found out");
+        }
+        println!("found out invalid");
+        return 0;
+    }
+    if let Some(ret) = *nodes[current_node].value.borrow() {
+        println!("cache hit in {current_node}: {ret}");
+        return ret;
+    }
+    if current_node == "dac" {
+        visited_dac = true;
+    }
+    if current_node == "fft" {
+        visited_fft = true;
     }
     let mut ret = 0;
-    for connection in &nodes[current_node] {
-        ret += find_out(nodes, &connection);
+    for connection in &nodes[current_node].connections {
+        ret += find_out(nodes, &connection, visited_dac, visited_fft);
+    }
+    if ret > 0 {
+        *nodes[current_node].value.borrow_mut() = Some(ret);
+        println!("cached for {current_node}: {ret}");
     }
     ret
+}
+
+struct Node {
+    connections: Vec<String>,
+    value: RefCell<Option<u32>>,
+}
+
+impl Node {
+    fn new(connections: Vec<String>) -> Self {
+        Node { connections, value: RefCell::new(None) }
+    }
 }
