d730f31240aee4a59b45978a889f5e1ac3b3e7a3
nba <nils.bauer@proetcon.de>
2025-12-18 16:33:44 +0100

    wip

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,201 +1,129 @@
 use std::fs;
-use std::str::FromStr;
-use std::sync::LazyLock;
 
 use regex::Regex;
 
 
 fn main() {
-    let input = fs::read_to_string("input1.txt").unwrap();
-    let mut res = 0;
-
-    for line in input.lines() {
-        let mut state : ButtonState = line.parse().unwrap();
-        if let Some(num_presses) = state.solve() {
-            println!("solved in {num_presses} presses");
-            res += num_presses;
-        } else {
-            panic!("couldn't solve");
-        }
-    }
-    println!("{res}");
-}
-
-#[derive(Debug,Clone)]
-struct ButtonState {
-    buttons: Vec<Vec<usize>>,
-    presses: Vec<u32>,
-    joltage_targets: Vec<u32>,
-    current_joltage: Vec<u32>,
-}
-
-impl ButtonState {
-    fn determine_next_button(&self) -> Option<usize> {
-        for button_size in (0 ..= self.buttons[0].len()).rev() {
-            if let Some((ret, _, _)) =
-                self.buttons.iter()
-                    .zip(self.calc_possible_button_presses())
-                    .enumerate()
-                    .map(|(idx, (button, poss_presses))| (idx, button, poss_presses))
-                    .filter(|(_, b, _)| b.len() == button_size)
-                    .max_by_key(|(_, _, p)| *p)
-            {
-                if ret > 0 {
-                    return Some(ret);
-                }
-            }
-        }
-        None
-    }
-
-    fn solve(&mut self) -> Option<u32> {
-        let mut ret = 0;
-        while let Some(next_button) = self.determine_next_button() {
-            println!("trying to press button {next_button}");
-            self.press_button(next_button);
-            println!("joltages: {:?}", self.current_joltage);
-            ret += 1;
-        }
-        if self.cmp_joltage() {
-            Some(ret)
-        } else {
-            None
-        }
-    }
-
-    fn press_button(&mut self, idx: usize) {
-        self.presses[idx] += 1;
-        for joltage in &self.buttons[idx] {
-            self.current_joltage[*joltage] += 1;
-        }
-    }
-
-    fn cmp_joltage(&self) -> bool {
-        let mut joltages = vec![0; self.joltage_targets.len()];
-        for (button_joltages, presses) in self.buttons.iter().zip(self.presses.iter()) {
-            for button_joltage in button_joltages {
-                joltages[*button_joltage] += presses;
-            }
-        }
-        joltages.iter().zip(self.joltage_targets.iter()).all(|(joltage, target)| *joltage == *target)
-    }
-
-    //fn exceeds_joltage(&self) -> bool {
-    //    let mut joltages = vec![0; self.joltage_targets.len()];
-    //    for (button_joltages, presses) in self.buttons.iter().zip(self.presses.iter()) {
-    //        for button_joltage in button_joltages {
-    //            joltages[*button_joltage] += presses;
-    //        }
-    //    }
-    //    joltages.iter().zip(self.joltage_targets.iter()).any(|(joltage, target)| *joltage > *target)
-    //}
-
-    fn calc_possible_button_presses(&self) -> Vec<u32> {
-        let ret = self.buttons.iter()
-            .map(|b| b.iter()
-                .map(|j| self.joltage_targets[*j] - self.current_joltage[*j])
-                .min()
-                .unwrap())
-            .collect();
-        println!("possible: {ret:?}");
-        ret
-    }
-}
-
-impl FromStr for ButtonState {
-    type Err = ButtonStateParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"(?x)
+    let re = Regex::new(r"(?x)
 \[(?P<indicator>[\.\#]+)\]\s*
 (?P<wiring>[(),0-9\ ]+)\s*
 \{(?P<joltage>[\{\}\,0-9]+)\}
-    ").unwrap());
+    ").unwrap();
+    let input = fs::read_to_string("input2.txt").unwrap();
 
-        let caps = RE.captures(s).unwrap();
+
+    for line in input.lines() {
+        println!();
+        let caps = re.captures(line).unwrap();
         let _indicator = &caps["indicator"];
         let wiring = &caps["wiring"].trim();
-        let joltage = &caps["joltage"];
+        let joltages = &caps["joltage"];
 
-        let mut buttons: Vec<_> =
-            wiring.split(' ')
-            .map(|b| {
+        let buttons : Vec<_> = wiring
+            .split(' ')
+            .map(|b|
                 b.trim_matches(&['(', ')'])
                  .split(',')
-                 .map(|d| d.parse::<usize>().unwrap())
+                 .map(|d| d.parse::<i32>().unwrap())
                  .collect::<Vec<_>>()
-            })
-        .collect();
-        buttons.sort_by_key(|b| b.len());
-        buttons.reverse();
-        let presses = vec![0; buttons.len()];
+            )
+            .collect();
+        let joltages : Vec<_> = joltages.split(',').map(|d| d.parse::<i32>().unwrap()).collect();
 
-        let joltage_targets : Vec<_> = joltage.split(',').map(|j| j.parse().unwrap()).collect();
-        let current_joltage = vec![0; joltage_targets.len()];
-        println!("buttons: {buttons:?}");
-        Ok(ButtonState { buttons, presses, joltage_targets, current_joltage })
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-enum ButtonStateParseError {
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    //#[test]
-    //fn tr() {
-    //    let buttons = vec![[3], [2], [1, 3], [2, 3], [0, 2], [0, 1]];
-    //    let possible = vec![7, 4, 5, 4, 3, 3];
-    //    let button_size = 1;
-    //    self.buttons.iter()
-    //        .zip(self.calc_possible_button_presses())
-    //        .enumerate()
-    //        .map(|(idx, (button, poss_presses))| (idx, button, poss_presses))
-    //        .filter(|(_, b, _)| b.len() == button_size)
-    //        .max_by_key(|(_, _, p)| *p)
-    //}
-
-    #[test]
-    fn tr2() {
-        let data = vec![vec![0,2,3,4],vec![2,3],vec![0,4],vec![0,1,2],vec![1,2,3,4]];
-        get_next_idx(&data, Vec::new());
-    }
-
-    fn get_next_idx(data: &Vec<Vec<usize>>, factors: Vec<i32>) {
-        if factors.len() < 5 {
-            for factor in -1..=2 {
-                let mut factors = factors.clone();
-                factors.push(factor);
-                get_next_idx(data, factors);
-            }
-        } else {
-            for (idx, factor) in factors.iter().enumerate() {
-                let term = match factor {
-                    -1 => format!("- x{idx} "),
-                    0  => format!("+0x{idx} "),
-                    1  => format!("+ x{idx} "),
-                    f  => format!("+ {f}x{idx} "),
-                };
-                print!("{term}");
-            }
-            print!(" = ");
-            let mut result = vec![0; 5];
-            for (factor, data) in factors.iter().zip(data.iter()) {
-                for d in data {
-                    result[*d] += factor;
+        let mut matrix = Vec::new();
+        for (joltage_idx, joltage) in joltages.iter().enumerate() {
+            let mut row = vec![0; buttons.len()];
+            for (button_idx, button) in buttons.iter().enumerate() {
+                for wire in button {
+                    if *wire == joltage_idx.try_into().unwrap() {
+                        row[button_idx] = 1;
+                    }
                 }
             }
-            for res in &result {
-                print!("{res:>2} ");
+            row.push(*joltage);
+            matrix.push(row);
+        }
+        println!("Matrix:\n");
+        print_matrix(&matrix);
+
+        let mut target_matrix = Vec::new();
+
+        // find defining row for each variable
+        for col_idx in 0..buttons.len() {
+            if let Some(row_idx) = find_row_for_variable(&matrix, col_idx) {
+                println!("\nrow for column {}: {}", col_idx+1, row_idx+1);
+                let mut row = matrix.swap_remove(row_idx);
+                let factor = row[col_idx];
+                for value in row.iter_mut() {
+                    *value /= factor;
+                }
+
+                println!("\nnormalize remaining rows:");
+                for matrix_row in matrix.iter_mut() {
+                    if matrix_row[col_idx] != 0 {
+                        let factor = matrix_row[col_idx];
+                        for (col_idx, value) in matrix_row.iter_mut().enumerate() {
+                            *value -= row[col_idx] * factor;
+                        }
+                    }
+                }
+                print_matrix(&matrix);
+
+                target_matrix.push(Some(row));
+            } else {
+                println!("no row found for column {}", col_idx+1);
+                target_matrix.push(None);
             }
-            let num0 = result.iter().fold(0, |acc, x| if *x == 0 { acc + 1} else { acc });
-            if num0 == 4 {
-                print!("HIERHIERHIER");
-            }
-            println!("   | {}", num0);
+            println!("\nsaved rows:");
+            print_option_matrix(&target_matrix);
+        }
+
+        let additional_vars = 
+        println!("------------------------------------------");
+    }
+}
+
+fn find_row_for_variable(matrix: &Vec<Vec<i32>>, column: usize) -> Option<usize> {
+    for (row_idx, row) in matrix.iter().enumerate() {
+        if row[column] != 0 {
+            return Some(row_idx);
         }
     }
+    None
+}
+
+fn print_matrix(matrix: &Vec<Vec<i32>>) {
+    for row in matrix {
+        print!("| ");
+        for value in row {
+            print!("{value:>3} ");
+        }
+        println!("|");
+    }
+}
+
+fn print_option_matrix(matrix: &Vec<Option<Vec<i32>>>) {
+    let mut row_len = None;
+    for row in matrix {
+        if let Some(row) = row {
+            row_len.get_or_insert(row.len());
+            print!("| ");
+            for value in row {
+                print!("{value:>3} ");
+            }
+            println!("|");
+        } else {
+            let row_len = row_len.get_or_insert(find_row_len(matrix).unwrap_or(0));
+            println!("| {}|", "    ".repeat(*row_len));
+        }
+    }
+}
+
+fn find_row_len(matrix: &Vec<Option<Vec<i32>>>) -> Option<usize> {
+    for row in matrix {
+        if let Some(row) = row {
+            return Some(row.len());
+        }
+    }
+    None
 }
